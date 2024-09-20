ddace367ab30e9b5eb340591f93c93d019510bb0
Anis Derriche <anis.derriche@ynov.com>
2024-09-20 14:35:26 +0200

    site internet ymmersions

---
+++ site_internet/script.js
@@ -0,0 +1,10 @@
+
+btn1.onclick = () => document.body.style.backgroundColor = "blue"
+btn2.onclick = () => document.body.style.backgroundColor = "violet"
+btn3.onclick = () => document.body.style.backgroundColor = "red"
+btn4.onclick = () => document.body.style.backgroundColor = "pink"
+btn5.onclick = () => document.body.style.backgroundColor = "yellow"
+btn6.onclick = () => document.body.style.backgroundColor = "rgb(0, 0, 35)"
+
+
+
